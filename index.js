f13566b729776c94941e90277c72529acd90e0a6
Nathan Baker <pcnate@gmail.com>
2016-08-02 22:08:45 -0500

    server loads configuration

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const fs            = require("fs");
 const EventEmmitter = require("events");
 
-class serverEmitter extends EventEmmitter {}
+const configFilePath = "config.json";
+
+class ServerEmitter extends EventEmmitter {}
+const serverEmitter = new ServerEmitter();
+
+serverEmitter.on('startService', function( config ) {
+
+  console.log( config );
+
+  var io = require("socket.io").listen(8080);
+
+  var other_server = require("socket.io-client")( config.protocol + "://" + config.server + ":" + config.port );
+
+  other_server.on("connect", function() {
+    console.log('connected to other server');
+  })
+
+  io.sockets.on("connection", function( socket ) {
+  })
+
+})
 
 /**
  *  exit the application if no configuration file is found
@@ -13,15 +33,9 @@ if( !fs.existsSync("config.json") ) {
   require("process").exit();
 } else {
 
+  let config = fs.readFileSync( configFilePath );
+  config = JSON.parse( config );
+
+  serverEmitter.emit('startService', config );
+
 }
-
-var io = require("socket.io").listen(8080);
-
-var other_server = require("socket.io-client")("http://128.10.100.92:3000");
-
-other_server.on("connect", function() {
-  console.log('connected to other server');
-})
-
-io.sockets.on("connection", function( socket ) {
-})
