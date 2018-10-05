750f4bfc3c43fd0a15a80edf0924a32b5cf51c70
Nathan Baker <pcnate@gmail.com>
2018-10-05 17:10:39 -0500

    remove express, upload via form-data instead of spawning curl

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs            = require("fs");
 const EventEmmitter = require("events");
 const spawn         = require("child_process").spawn;
 const schedule      = require("node-schedule");
+const https         = require('https');
+const FormData      = require('form-data');
 
 const configFilePath = "config.json";
 var config = {
@@ -15,6 +17,7 @@ var delay = 10;
 
 var image = '';
 var uploadImage = '';
+var baseUploadUrl = '';
 
 var dt = new Date();
 var secInterval = setInterval( () => {
@@ -25,11 +28,11 @@ var secInterval = setInterval( () => {
   } else {
     console.log( delay - (dt.getSeconds() % delay) );
   }
-},1000);
+}, 1000 );
 
 serverEmitter.on('startCamera', () => {
   var imageTime = new Date();
-  // var args = ['-vf', '-hf', '-w', '1920', '-h', '1080', '-o', '/dev/shm/current.jpg', '-t', '999999999', '-tl', '10000', '-v'];
+
   var args = [
     // '-v',
     '-vf',
@@ -52,109 +55,27 @@ serverEmitter.on('startCamera', () => {
     console.log( 'raspistill ended with code:', code );
   })
 
+  // watch for changes on the file and upload them to the server
   fs.watchFile('/dev/shm/current.jpg', ( current, previous ) => {
+
     console.log('taking new image');
-    imageTime = new Date();
-    fs.readFile('/dev/shm/current.jpg', ( err, data ) => {
-      image = data;
+    unixTimestamp = Math.round( ( new Date() ).getTime() / 1000 );
 
-      // if( upload ) {
+    console.log('uploading new image');
 
-        // upload = false;
-        console.log('uploading new image');
+    var form = new FormData();
 
-        uploadImage = image;
-        serverEmitter.emit('startImageUpload');
+    form.append( 'filedata', fs.createReadStream( '/dev/shm/current.jpg' ) );
 
-        // console.log( config )
+    let uploadPath = config.baseUploadUrl + '/' + unixTimestamp;
 
-        fs.writeFile('/dev/shm/upload.jpg', image, () => {
-          if( config.upload === 'y' ) {
+    form.submit( uploadPath, ( error, response ) => {
+      console.log( 'done uploading' );
+    } )
 
-            let uploadUrl = config.protocol + '://' + config.server + (config.port === '' ? '' : ':' + config.port) + '/upload/' + config.deviceID;
-            var uploadImage = spawn('curl', [ '-i', '-F', 'filedata=@/dev/shm/upload.jpg', uploadUrl, '-o', '/dev/shm/cam-curl.log' ]);
-            uploadImage.on('close', () => {
-              console.log('uploading new image done');
-              serverEmitter.emit('imageUploaded');
-            })
-          }
-        })
-
-      // }
-      serverEmitter.emit('imageRefresh');
-    })
   })
 })
 
-
-serverEmitter.on('startService', () => {
-
-  console.log( config );
-
-  // setup the server
-  var express = require("express");
-  var app = express();
-  var http = require("http").Server(app);
-  var io = require("socket.io")(http);
-
-  // root index
-  app.get('/', ( req, res ) => {
-    res.sendFile('webapp/index.html', { root: __dirname });
-  })
-
-  // static image files
-  app.use('/images', express.static( __dirname + '/webapp/images' ) );
-
-  // image is loaded from memory
-  app.get('/current.jpg', ( req, res ) => {
-
-    if( image && image.length > 0 ) {
-      res.writeHead( 200, { 'Content-Type': 'image/jpeg' } );
-      res.write( image );
-      res.end();
-    } else {
-      res.writeHead( 400, { 'Content-Type': 'text/plain' } );
-      res.write( 'No image available' );
-      res.end();
-    }
-
-  })
-
-  http.listen( 3000 , () => {
-    console.log("listening on port 3000");
-  })
-
-  var client = require('socket.io-client')( config.protocol + '://' + config.server + ( config.port === '' ? '' : ':' + config.port ) );
-
-  // TODO need to detect current connection
-  client.on('connect', () => {
-
-    console.log('connected to other server');
-
-    serverEmitter.on('startImageUpload', () => {
-      client.emit('')
-    })
-
-    serverEmitter.on('imageUploaded', () => {
-      client.emit('imageRefresh');
-    })
-
-    client.on('disconnect', () => {
-      console.log('disconnected from server');
-    })
-
-  })
-
-  //
-  io.sockets.on('connection', function( socket ) {
-    console.log('client connected');
-    serverEmitter.on('imageRefresh', function() {
-      console.log('telling client to load new image');
-      socket.emit('imageRefresh');
-    })
-  })
-
-})
 
 /**
  *  exit the application if no configuration file is found
@@ -169,6 +90,6 @@ if( !fs.existsSync('config.json') ) {
   let configJSON = fs.readFileSync( configFilePath );
   config = JSON.parse( configJSON );
 
-  serverEmitter.emit('startService' );
+  config.baseUploadUrl = config.protocol + '://' + config.server + ( config.port === '' ? '' : ':' + config.port ) + '/upload/' + config.deviceID;
 
 }
