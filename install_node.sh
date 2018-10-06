7bc69e870c0e59cded8081f67eaf0eadf5c94fb2
Nathan Baker <pcnate@gmail.com>
2018-10-06 01:53:49 -0500

    node update script

--- install_node.sh
+++ install_node.sh
@@ -4,3 +4,13 @@
 
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install -y nodejs build-essential
+
+
+node -v
+npm -v
+
+npm cache clean -f
+npm install -g n
+sudo n stable
+
+echo 'now exit and reconnect and run node -v && npm -v'
